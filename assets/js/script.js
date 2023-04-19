4e87ba6c4482da6f62f3366911ded0d1f1b9b783
Camilla Centio <camillacentio@gmail.com>
2023-04-19 12:42:43 +0000

    Add multiply and division code

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,10 +25,13 @@ function runGame(gameType) {
     displayAdditionQuestion(num1, num2); 
  } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
-
+ } else if (gameType === "subtract") {
+    displaySubtractQuestion(num1, num2);
+ } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2);
  } else {
-    alert(`unknown gametype: ${gameType}`);
-    throw `unknown gametype: ${gameType}, Aborting!`;
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
  }
 }
 /** Checks the answer against the first element in
@@ -64,7 +67,7 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-        else {
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}, Aborting!`;
     }
@@ -92,20 +95,25 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "-";
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
 
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
 
 function displayDivisionQuestion(operand1, operand2) {
     
-        document.getElementById('operand1').textContent = operand1;
-        document.getElementById('operand2').textContent = operand2;
-        document.getElementById('operator').textContent = "x";
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 
 }
-
-
