bfc601de14db507ab0bca52ffb2a865d51f60c28
Camilla Centio <camillacentio@gmail.com>
2023-04-19 11:40:58 +0000

    Add operand code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
 
     let buttons = document.getElementsByTagName("button");
     for (let button of buttons) {
@@ -7,24 +7,25 @@ document.addEventListener("DOMContentLoaded", function(){
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                rungame("gametype");
+                runGame(gameType);
             }
-        })
+        });
     }
-    rungame("addition"); 
-})
+        runGame("addition"); 
+});
 /**
  * The main game "loop", called when script is first loaded
  * and after the user's answer has been processed
  */
-function rungame(gametype) {
+function runGame(gameType) {
  let num1 = Math.floor(Math.random() * 25) + 1;
  let num2 = Math.floor(Math.random() * 25) + 1;
 
- if (gametype === "addition"){
+ if (gameType === "addition"){
     displayAdditionQuestion(num1, num2); 
  } else {
-    alert('unknown gametype: ${gametype}')
+    alert(`unknown gametype: ${gameType}`);
+    throw `unknown gametype: ${gameType}, Aborting!`;
  }
 }
 
@@ -44,7 +45,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
